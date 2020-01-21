a17145ac59e99d95d3de01e58b9e6ba5730c7e0d
jinah <jinah0818@gmail.com>
2020-01-22 08:52:31 +0900

    3_Practice(events, if)

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
+const age = prompt("How old are you?");
+
+if(age >= 18 && age <= 21){
+    console.log("you can drink, but you should not");
+}else if(age > 21) {
+    console.log("go ahead");
+}else {
+    console.log("too young");
+}
 const title = document.querySelector("#title");
 
+
+function handleClick() {
+    title.style.color = "blue";
+    // title.style.color = "red";
+}
+// function handleResize(){
+//     console.log(event);
+//     console.log('I have been resize');
+// }
+
+title.addEventListener("click", handleClick);
+// window.addEventListener("resize", handleResize);
+
+
 // const title = document.getElementById("title");
-title.innerHTML = "Hi, From JS";
-title.style.color = 'red';
-document.title = "I own you now";
+// title.innerHTML = "Hi, From JS";
+// title.style.color = 'red';
+// document.title = "I own you now";
 
 
 // console.dir(document);
